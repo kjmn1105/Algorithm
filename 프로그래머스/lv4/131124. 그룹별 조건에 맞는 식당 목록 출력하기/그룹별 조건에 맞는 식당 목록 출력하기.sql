d8c36a39dc326639a4244ef3e7fd604b2f4e1942
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-07-08 22:54:38 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,29 +1,14 @@
-
-
-# select 
-#     MEMBER_NAME,
-#     REVIEW_TEXT,
-#     date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-# from MEMBER_PROFILE t1
-# join REST_REVIEW t2 on t1.MEMBER_ID = t2.MEMBER_ID
-# where (t1.MEMBER_ID) in (select MEMBER_ID from temp having max(CNT))
-# order by 3, 2
-
-with temp1 as (
-    select MEMBER_ID, count(*) REVIEW_COUNT
-    from REST_REVIEW
-    group by MEMBER_ID    
+with member_review_cnt as (
+select MEMBER_ID, count(*) REVIEW_CNT
+from REST_REVIEW
+group by MEMBER_ID
 )
 
-select
-    MEMBER_NAME,
-    REVIEW_TEXT,
-    date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-from MEMBER_PROFILE t1
-join REST_REVIEW t2 on t1.MEMBER_ID = t2.MEMBER_ID
-where t2.MEMBER_ID in (
+select MEMBER_NAME, REVIEW_TEXT, left(REVIEW_DATE, 10) REVIEW_DATE
+from MEMBER_PROFILE M, REST_REVIEW R
+where M.MEMBER_ID = R.MEMBER_ID and M.MEMBER_ID in (
     select MEMBER_ID
-    from temp1
-    where REVIEW_COUNT = (select max(REVIEW_COUNT) from temp1)
-)
+    from member_review_cnt T1
+    where REVIEW_CNT = (select max(REVIEW_CNT) from member_review_cnt T2)
+    )
 order by 3, 2
