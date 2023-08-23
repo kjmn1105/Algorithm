c166df80a6b9e871b6159554dc3c3bc8c0322546
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-08-23 12:09:31 +0700

    [Silver III] Title: 통계학, Time: 400 ms, Memory: 57624 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -5,15 +5,18 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 nums = sorted([int(input().rstrip()) for _ in range(N)])
 
+# mean
+print(int(round(sum(nums)/N)))
+
+# median
+print(nums[N//2])
+
+# most_frequent
 if N==1:
     print(nums[0])
-    print(nums[0])
-    print(nums[0])
-    print(0)
-
-else :
-    print(int(round(sum(nums)/N)))
-    print(nums[N//2])
+else:
     a, b = Counter(nums).most_common()[:2]
     print(b[0] if a[1]==b[1] else a[0])
-    print(nums[-1] - nums[0])
+
+# range
+print(nums[-1] - nums[0])
