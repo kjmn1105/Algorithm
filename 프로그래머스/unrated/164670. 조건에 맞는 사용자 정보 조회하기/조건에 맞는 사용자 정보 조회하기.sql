19597cc91bbb754c933b2adbe89fed30992dc9fa
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-08-31 00:49:09 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,11 +1,33 @@
 select
     USER_ID,
     NICKNAME,
-    concat(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) as 전체주소,
-    concat(left(TLNO, 3), '-', substr(TLNO, 4, 4), '-', right(TLNO, 4)) as 전화번호
-from USED_GOODS_USER as t1
-join USED_GOODS_BOARD as t2
-on t1.USER_ID = t2.WRITER_ID
-group by USER_ID
-having count(USER_ID) >= 3
-order by USER_ID desc
+    concat(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) 전체주소,
+    concat(left(TLNO, 3), '-', substr(TLNO, 4, 4), '-', right(TLNO, 4)) TLNO
+from USED_GOODS_USER
+where USER_ID in (
+    select WRITER_ID
+    from USED_GOODS_BOARD
+    group by WRITER_ID
+    having count(*) >= 3
+)
+order by 1 desc
+
+
+
+
+
+
+
+
+
+# select
+#     USER_ID,
+#     NICKNAME,
+#     concat(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) as 전체주소,
+#     concat(left(TLNO, 3), '-', substr(TLNO, 4, 4), '-', right(TLNO, 4)) as 전화번호
+# from USED_GOODS_USER as t1
+# join USED_GOODS_BOARD as t2
+# on t1.USER_ID = t2.WRITER_ID
+# group by USER_ID
+# having count(USER_ID) >= 3
+# order by USER_ID desc
