f1f9bfbfdc5c27c44a611c3c02916f8c87144065
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-09-14 10:54:32 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,4 +1,7 @@
-select t1.ANIMAL_ID, t1.ANIMAL_TYPE, t1.NAME
+select 
+    t1.ANIMAL_ID, 
+    t1.ANIMAL_TYPE, 
+    t1.NAME
 from ANIMAL_INS t1
 join ANIMAL_OUTS t2 on t1.ANIMAL_ID = t2.ANIMAL_ID
 where SEX_UPON_INTAKE like 'Intact%' and SEX_UPON_OUTCOME not like 'Intact%'
