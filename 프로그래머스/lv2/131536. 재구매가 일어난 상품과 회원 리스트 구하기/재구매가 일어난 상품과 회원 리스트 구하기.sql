932d3e30016bb08409256ee5e4b4d75fbd93049f
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-09-16 16:10:08 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/lv2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,6 +1,13 @@
-select 
-    USER_ID, PRODUCT_ID
+# desc ONLINE_SALE
+select USER_ID, PRODUCT_ID
 from ONLINE_SALE
 group by USER_ID, PRODUCT_ID
-having count(*) > 1
+having count(*)>1
 order by 1, 2 desc
+
+# select 
+#     USER_ID, PRODUCT_ID
+# from ONLINE_SALE
+# group by USER_ID, PRODUCT_ID
+# having count(*) > 1
+# order by 1, 2 desc
