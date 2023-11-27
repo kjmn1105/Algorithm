ad1a72152ec139946baa91a3e9cbe278599ae042
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-11-27 20:20:25 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 856 ms, Memory: 66932 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -8,16 +8,13 @@ cnt = 0
 for _ in range(N):
     l, p = map(int, input().split())
 
-    # p보다 크면 손가락은 뗀다.
+    # l번 줄에서 p번 프렛보다 큰 곳은 다 손가락을 뗀다.
     while temp[l] and (temp[l][-1] > p):
         temp[l].pop()
         cnt += 1
 
-    if temp[l] and temp[l][-1]==p:
-        pass
-
-    # p보다 작거나 비어있으면, 손가락을 추가한다.
-    else :
+    # l번 줄이 비어있거나 가장 큰 수의 프렛이 p보다 작으면 손가락을 추가한다.
+    if (not temp[l]) or temp[l][-1] < p:
         temp[l].append(p)
         cnt += 1
 
