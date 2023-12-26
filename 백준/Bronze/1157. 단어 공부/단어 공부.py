e79f6b6496f8bd91f401f6b8094ec8c5de241b56
kjmn1105 <88579133+kjmn1105@users.noreply.github.com>
2023-12-26 22:29:03 +0900

    [Bronze I] Title: 단어 공부, Time: 112 ms, Memory: 35176 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,6 +1,6 @@
+# counter 사용하기
 from collections import Counter
-counter = Counter(input().lower())
+counter = Counter(input().upper())
 common = counter.most_common()
 
-# 하나의 알파벳으로만 이루어진 단어를 주의해야 한다. (or RuntimeError)
-print("?" if (len(common)>1 and common[0][1]==common[1][1]) else common[0][0].upper())
+print("?" if (len(common)>1 and common[0][1]==common[1][1]) else common[0][0])
